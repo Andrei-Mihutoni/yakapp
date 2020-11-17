b8ecb10638693d945f8d7e12c6edb0f5f6ac6dc9
Andrei Mihutoni <andrei.mihutoni@gmail.com>
2020-11-17 12:10:53 +0100

    hooking up the fetching from restdb

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Main from "./components/Main";
 import './App.css';
 
 function App() {
-  const [cards, setCards] = useState([
-    {
-      task:"Clean room",
-      list:"todo",
-      highlight:true
-    },
-    {
-      task:"Clean kitchen",
-      list:"doing",
-      highlight:false
-    },
-    {
-      task:"Do JS",
-      list:"done",
-      highlight:true
-    },
-    {
-      task:"Learn React",
-      list:"doing",
-      highlight:false
-    }
-  ]);
-  
+  const [cards, setCards] = useState([]);
+
+  const key = "5f96aff04b77c1637d147e14";
+  const url = "https://carsdb-97db.restdb.io/rest/cards";
+
+  useEffect(() => {
+    fetch(url, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": key,
+        "cache-control": "no-cache"
+      }
+    })
+      .then(e => e.json())
+      .then(data => setCards(data));
+
+
+  }, []);
   const day = "monday".split("").join(" ");
   const message = "Hi Mom";
   const addCard = () => {
     setCards(cards.concat({
-      task:"Learn React " + Math.random(),
-      list:["todo", "doing", "done"][Math.floor(Math.random()*3)],
-      highlight:false
+      task: "Learn React " + Math.random(),
+      list: ["todo", "doing", "done"][Math.floor(Math.random() * 3)],
+      highlight: false
     }))
   }
   return (
     <>
-    <h1>Hello {`my friend it's ${day}`}</h1>
-    <button onClick={addCard}>Add Card</button>
-    <div className="App">
-      <Nav />
-      
-      <Main cards={cards} message={message}/>
-    </div>
+      <h1>Hello {`my friend it's ${day}`}</h1>
+      <button onClick={addCard}>Add Card</button>
+      <div className="App">
+        <Nav />
+
+        <Main cards={cards} message={message} />
+      </div>
     </>
   );
 }
 
-function Nav(){
+function Nav() {
   const [items, setItems] = useState([
-    "hi","mom","hotpink"
+    "hi", "mom", "hotpink"
   ])
-  function addItem(){
+  function addItem() {
     //create a copy
     const nextState = items.concat("Dannie")
 
@@ -63,10 +59,10 @@ function Nav(){
     <nav className="Nav">
       <h2>Hi im nav </h2>
       {
-        items.map((item, index)=><p key={index} >{item}</p>)
+        items.map((item, index) => <p key={index} >{item}</p>)
       }
-      
-      
+
+
       <button onClick={addItem}>Click me</button>
     </nav>
   )
